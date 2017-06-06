4a3f21551565fd1e1aed97fce94d94a434486fae
LT-036118\RWR730 <r.h.a.weijer@vu.nl>
2017-06-06 09:57:56 +0200

    Added the main file

---
+++ ScienceCounter.R
@@ -0,0 +1,8 @@
+#An empty R script that we can use as an example
+#It contains only a few lines
+
+Science_transmitters = 1
+Science_receivers = 15
+
+Total_Scientists = Science_transmitters + Science_receivers
+
